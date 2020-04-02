fc2162491d3418bdcda74f91beafb3aaa6bfabe4
thelol1mpo <thelol1mpo@gmail.com>
2020-04-02 10:48:29 +0700

    Update task6 to use threads

--- javaTask6/src/main/java/ru/sibsutis/App.java
+++ javaTask6/src/main/java/ru/sibsutis/App.java
@@ -6,24 +6,59 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(25565);
-        Socket client = server.accept();
+class ServerThread implements Runnable {
+    Socket client;
 
-        Scanner in = new Scanner(client.getInputStream());
-        PrintWriter out = new PrintWriter(client.getOutputStream());
+    ServerThread(Socket client) {
+        this.client = client;
+    }
 
+    @Override
+    public void run() {
+        Scanner in = null;
+        try {
+            in = new Scanner(client.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PrintWriter out = null;
+        try {
+            out = new PrintWriter(client.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert in != null;
         String str = in.nextLine();
         System.out.println(str);
 
-        String tmp ="HTTP/1.1 200 OK\n" + "Content-type: text/html\n" + "Content-Length: 149\n" + "\n" +
-                "<html><body><p>Hello</p></body></html>";
+        String tmp ="HTTP/1.1 200 OK\n" +
+                "Content-type: text/html\n" +
+                "Content-Length: 40\n" +
+                "\n" +
+                "<html><body><h1>Hello</h1></body></html>";
         System.out.println(tmp);
+        assert out != null;
         out.print(tmp);
         out.flush();
+        try {
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
-        client.close();
+public class App {
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(25565);
+        Socket client;
+        int clientNum = 0;
+        while (clientNum < 10) {
+            client = server.accept();
+            System.out.println("\n\nNew client");
+            clientNum++;
+            new Thread(new ServerThread(client)).start();
+        }
         server.close();
     }
 }
