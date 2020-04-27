53f43766a3d346fa51fcdac991954a66bb45ce8c
TheLol1mpo <thelol1mpo@gmail.com>
2020-04-27 16:15:23 +0700

    Next step in task 10

--- javaTask10/src/main/java/ru/sibsutis/Main.java
+++ javaTask10/src/main/java/ru/sibsutis/Main.java
@@ -1,5 +1,7 @@
 package ru.sibsutis;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -10,6 +12,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
 import java.util.Scanner;
 
 class ServerThread implements Runnable {
@@ -19,8 +22,24 @@ class ServerThread implements Runnable {
         this.client = client;
     }
 
+    public String formDataSting() throws IOException {
+        String response = "";
+        ObjectMapper mapper = new ObjectMapper();
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase db = mongoClient.getDatabase("user");
+        MongoCollection<Document> collection = db.getCollection("profile");
+        for (Document doc: collection.find()) {
+            response.concat("ass");
+        }
+        System.out.println(response);
+        return mapper.writeValueAsString(response);
+    }
+
     @Override
     public void run() {
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase db = mongoClient.getDatabase("user");
+        MongoCollection<Document> collection = db.getCollection("profile");
         Scanner in = null;
         try {
             in = new Scanner(client.getInputStream());
@@ -33,11 +52,23 @@ class ServerThread implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        assert in != null;
         String str = in.nextLine();
         System.out.println("Client request: " + str);
-        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-        MongoDatabase db = mongoClient.getDatabase("user");
-        MongoCollection<Document> collection = db.getCollection("profile");
+        if (str.contains("info")) {
+            StringBuilder response = new StringBuilder();
+            String JSON = "HTTP/1.1 200 OK\n" +
+                    "Content-type: application/json\n" +
+                    "\n";
+            for (Document doc : collection.find()) {
+                response.append(doc.toJson());
+                response.append("\n");
+            }
+            JSON += response.toString();
+            System.out.println(JSON);
+            out.print(JSON);
+            out.flush();
+        }
     }
 }
 
